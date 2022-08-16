8f18b4018460b57ccc010bf1dc0071cf49fd1f06
laurentbila <bin_bila@yahoo.fr>
2022-08-16 11:12:55 +0200

    navigation

---
+++ jsf-web-app/src/main/java/fr/m2i/jsf/web/app/UserData.java
@@ -0,0 +1,74 @@
+package fr.m2i.jsf.web.app;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+
+@ManagedBean(name = "userData", eager = true)
+public class UserData {
+
+    private static final List<Employee> employees = new ArrayList() {
+        {
+            add(new Employee("Anthony", "IT", 28, 3000.00));
+            add(new Employee("John", "Marketing", 42, 3700.00));
+            add(new Employee("Joe", "Sales", 21, 1800.00));
+        }
+    };
+
+    private String name;
+    private String department;
+    private int age;
+    private double salary;
+
+    public String addEmployee() {
+        
+        //Ajout d'un employé
+        Employee employee = new Employee(name, department, age, salary);
+        employees.add(employee);
+        
+        // Reset les données du formulaire
+        name = null;
+        department = null;
+        age = 0;
+        salary = 0d;
+        
+        //on affiche la table data
+        return null;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
